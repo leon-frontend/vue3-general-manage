ab05e30a59847cf51d2726ff01b50dba297561c0
leon <leon991214@gmail.com>
2024-09-13 14:39:56 +0800

    refactor: 新增 Logo 和 Menu 文件夹(开头大写)

---
+++ src/layout/Logo/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts" name="Logo">
+//
+import setting from '@/setting'
+</script>
+
+<template>
+  <div class="logo" v-if="setting.logoHidden">
+    <img :src="setting.logo" />
+    <p>{{ setting.title }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  width: 260px;
+  height: $base-menu-logo-height;
+  display: flex;
+  align-items: center;
+  padding: 12px;
+
+  img {
+    width: 40px;
+    height: 40px;
+    margin-right: 20px;
+  }
+
+  p {
+    font-size: $base-logo-title-fontsize;
+  }
+}
+</style>
